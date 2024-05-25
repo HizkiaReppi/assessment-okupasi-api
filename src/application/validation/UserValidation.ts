6164f64e1661a795a42d1b827d0f362949b4514a
Adhicitta Masran <adhicittamasran@gmail.com>
2024-05-25 13:55:30 +0800

    feat(user): add use case validation

---
+++ src/application/validation/UserValidation.ts
@@ -0,0 +1,28 @@
+import {ZodType, z} from 'zod';
+
+export class AkunValidation {
+  static readonly LOGIN: ZodType = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+  });
+
+  static readonly ADD: ZodType = z.object({
+    nama: z.string(),
+    email: z.string().email(),
+    password: z.string().min(8),
+  });
+
+  static readonly CHANGE_EMAIL: ZodType = z.object({
+    id: z.string().uuid(),
+    email: z.string().email(),
+  });
+
+  static readonly CHANGE_PASSWORD: ZodType = z.object({
+    id: z.string().uuid(),
+    password: z.string().min(8),
+  });
+
+  static readonly DELETE: ZodType = z.object({
+    id: z.string().uuid(),
+  });
+}
