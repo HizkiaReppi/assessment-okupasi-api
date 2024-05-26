bf27bf9d74bf2faf04a4a49e00b47a93cdb95aee
Adhicitta Masran <adhicittamasran@gmail.com>
2024-05-26 15:28:29 +0800

    refactor: rename and reconstruct authorizationMiddleware to grantSuperMiddleware

--- src/interface/http/middleware/authorization.ts
+++ src/interface/http/middleware/authorization.ts
@@ -1,16 +1,14 @@
 import {NextFunction, Request, Response} from 'express';
 import {AuthorizationError} from '../../../common/error/AuthorizationError';
 
-export const authorizationMiddleware = (roles: string[]) => {
-  return async (
-      req: Request,
-      res: Response,
-      next: NextFunction,
-  ) => {
-    if (!res.locals.access.is_super) {
-      next(new AuthorizationError());
-    }
+export const grantSuperMiddleware = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+  if (!res.locals.access.is_super) {
+    next(new AuthorizationError());
+  }
 
-    next();
-  };
+  next();
 };
