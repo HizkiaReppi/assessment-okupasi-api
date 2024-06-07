701bd0d1e210c63c4390a8f8c5d0732485396fc0
Adhicitta Masran <adhicittamasran@gmail.com>
2024-06-07 18:15:58 +0800

    feat(kompetensi-lulusan): add usecase

---
+++ src/application/usecase/kompetensi-lulusan/GetAllUsecase.ts
@@ -0,0 +1,28 @@
+import {KompetensiLulusanRepository}
+  from '../../../domain/kompetensi_lulusan/KompetensiLulusanRepository';
+import {GetAllKompetensiLulusanByIdSekolahInput}
+  from '../../../domain/kompetensi_lulusan/entity/kompetensi-lulusan';
+import {SekolahRepository} from '../../../domain/sekolah/SekolahRepository';
+import {Pagination} from '../../../util/pagination';
+
+export class GetAllKompetensiLulusanUsecase {
+  constructor(
+    private readonly kompetensiLulusanRepo: KompetensiLulusanRepository,
+    private readonly sekolahRepo: SekolahRepository,
+  ) {}
+
+  async execute(payload: GetAllKompetensiLulusanByIdSekolahInput) {
+    await this.sekolahRepo.verify(payload.id_sekolah);
+
+    payload.limit = payload.limit ? payload.limit : 10;
+    payload.page = payload.page ? payload.page : 1;
+
+
+    const [totalResult, data] = await this.kompetensiLulusanRepo
+        .getAllByIdSekolah(payload);
+
+    const {limit, page} = payload;
+
+    return new Pagination({limit, page, totalResult, data});
+  }
+}
