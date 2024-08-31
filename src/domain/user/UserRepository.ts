1088ab2fbf76222cc3c18e753b787a2ba327f2b5
Adhicitta Masran <adhicittamasran@gmail.com>
2024-09-01 03:10:02 +0800

    feat(user): change user table name to users

--- src/domain/user/UserRepository.ts
+++ src/domain/user/UserRepository.ts
@@ -1,11 +1,11 @@
-import {User} from '@prisma/client';
+import {Users} from '@prisma/client';
 import {AddUserInput, LoginOutput} from './entity/user';
 
 export interface UserRepository {
   add(req: AddUserInput): Promise<void>
   login(email: string): Promise<LoginOutput>
-  getById(id: string): Promise<User>
-  getAll(): Promise<User[]>
+  getById(id: string): Promise<Users>
+  getAll(): Promise<Users[]>
   changeEmail(id: string, email: string): Promise<void>
   changePassword(id: string, password: string): Promise<void>
   deleteById(id: string): Promise<void>
