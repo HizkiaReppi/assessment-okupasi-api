5310a3d7a10955846dcb62566d4a69c2ccff6600
Adhicitta Masran <adhicittamasran@gmail.com>
2024-08-31 16:45:00 +0800

    feat(user): change db provider to mysql and change user model to users

--- prisma/migrations/20240525055729_add_authentication_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "authentication" (
-    "token" VARCHAR(300) NOT NULL,
-    "expires_at" TIMESTAMP(3) NOT NULL,
-    "is_used" BOOLEAN NOT NULL DEFAULT false,
-
-    CONSTRAINT "authentication_pkey" PRIMARY KEY ("token")
-);
--- prisma/migrations/20240528061327_add_okupasi_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "okupasi" (
-    "kode" TEXT NOT NULL,
-    "nama" TEXT NOT NULL,
-
-    CONSTRAINT "okupasi_pkey" PRIMARY KEY ("kode")
-);
-
-CREATE UNIQUE INDEX "okupasi_nama_key" ON "okupasi"("nama");
