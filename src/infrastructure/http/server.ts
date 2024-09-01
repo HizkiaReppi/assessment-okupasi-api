b561c90bc84713a0dc4c16c148c8c160b120eed1
Hizkia Reppi <hizkiajefren0501@gmail.com>
2024-09-01 14:12:56 +0800

    fix(cors): can't read origin value

--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -1,32 +1,33 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import {errorMiddleware} from '../../interface/http/middleware/error';
-import {authenticationRouter}
-  from '../../interface/http/api/authentication/router';
-import {userRouter} from '../../interface/http/api/user/router';
-import {okupasiRouter} from '../../interface/http/api/okupasi/router';
-import {sekolahRouter} from '../../interface/http/api/sekolah/router';
 import cors from 'cors';
+import { errorMiddleware } from '../../interface/http/middleware/error';
+import { authenticationRouter } from '../../interface/http/api/authentication/router';
+import { userRouter } from '../../interface/http/api/user/router';
+import { okupasiRouter } from '../../interface/http/api/okupasi/router';
+import { sekolahRouter } from '../../interface/http/api/sekolah/router';
 
 export const initServer = () => {
   const app = express();
 
-  app.get('', function(req, res) {
-    res.status(200).json({message: 'Welcome to Assessment Okupasi API'});
+  app.get('', function (req, res) {
+    res.status(200).json({ message: 'Welcome to Assessment Okupasi API' });
   });
 
-  app.use(cors({
-    origin: process.env.origin,
-    credentials: true,
-  }));
+  app.use(
+    cors({
+      origin: process.env.ORIGIN,
+      credentials: true,
+    }),
+  );
   app.use(bodyParser.json(), cookieParser());
   app.use(
-      '/api/v1',
-      authenticationRouter(),
-      userRouter(),
-      okupasiRouter(),
-      sekolahRouter(),
+    '/api/v1',
+    authenticationRouter(),
+    userRouter(),
+    okupasiRouter(),
+    sekolahRouter(),
   );
   app.use(errorMiddleware);
 
