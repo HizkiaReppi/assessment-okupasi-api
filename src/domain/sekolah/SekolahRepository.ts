b5ff10cae4f597f8b43b20b97a085da969098e8b
Adhicitta Masran <adhicittamasran@gmail.com>
2024-09-18 19:54:45 +0800

    feat(sekolah): add konsentrasi sekolah to the response

--- src/domain/sekolah/SekolahRepository.ts
+++ src/domain/sekolah/SekolahRepository.ts
@@ -1,14 +1,14 @@
-import {Sekolah} from '@prisma/client';
 import {
   AddSekolahInput,
   EditSekolahInput,
   GetAllSekolahInput,
+  GetSekolahOutput,
 } from './entity/sekolah';
 
 export interface SekolahRepository {
   add(data: AddSekolahInput): Promise<string>
-  getAll(req: GetAllSekolahInput): Promise<[number, Sekolah[]]>
-  getById(id: string): Promise<Sekolah>
+  getAll(req: GetAllSekolahInput): Promise<[number, GetSekolahOutput[]]>
+  getById(id: string): Promise<GetSekolahOutput>
   editById(id: string, data: EditSekolahInput): Promise<void>
   deleteById(id: string): Promise<void>
   verify(id: string): Promise<void>
